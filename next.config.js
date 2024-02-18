829e3d7225a028f1147a0c327e9b75745be96b8f
nalse <54538144+nalse@users.noreply.github.com>
2024-02-19 00:48:56 +0400

    Update next.config.js
    server url

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
       {
         source: "/",
         headers: [
-          { key: "Access-Control-Allow-Origin", value: "https://client.meliordism.az" },
+          { key: "Access-Control-Allow-Origin", value: "https://server.meliordism.az" },
           { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,DELETE,PATCH,OPTIONS" },
           { key: "Access-Control-Allow-Headers", value: "*" },
           { key: "Access-Control-Allow-Credentials", value: "true" },
